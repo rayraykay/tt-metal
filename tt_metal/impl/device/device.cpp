45b69fc29e98067e008224c1f8a7e08d69498f17
Almeet Bhullar <abhullar@tenstorrent.com>
2025-02-10 18:02:40 -0500

    debug virtual

--- tt_metal/impl/device/device.cpp
+++ tt_metal/impl/device/device.cpp
@@ -751,9 +751,18 @@ void Device::initialize_and_launch_firmware() {
 
     int non_worker_cores_idx = 0;
     for (const tt::umd::CoreCoord& core : pcie_cores) {
+        std::cout << "pcie core is " << core.str() << " virtual is "
+                  << tt::Cluster::instance().get_virtual_coordinate_from_physical_coordinates(this->id(), core).str()
+                  << std::endl;
+        tt::umd::CoreCoord translated_coord =
+            soc_d.translate_coord_to(core, CoordSystem::PHYSICAL, CoordSystem::VIRTUAL);
+        std::cout << "\tupdated to " << translated_coord.str() << std::endl;
         core_info->non_worker_cores[non_worker_cores_idx++] = {core.x, core.y, AddressableCoreType::PCIE};
     }
     for (const tt::umd::CoreCoord& core : dram_cores) {
+        std::cout << "dram core is " << core.str() << " virtual is "
+                  << tt::Cluster::instance().get_virtual_coordinate_from_physical_coordinates(this->id(), core).str()
+                  << std::endl;
         core_info->non_worker_cores[non_worker_cores_idx++] = {core.x, core.y, AddressableCoreType::DRAM};
     }
     for (const tt::umd::CoreCoord& core : eth_cores) {
@@ -761,6 +770,7 @@ void Device::initialize_and_launch_firmware() {
     }
     if (hal_ref.is_coordinate_virtualization_enabled()) {
         // Track Virtual Non Worker Cores (In this case only Eth) separately
+        // todo this needs to include pcie and dram for bh!
         uint32_t virtual_non_worker_cores_idx = 0;
         for (const tt::umd::CoreCoord& core : eth_cores) {
             auto virtual_core = this->virtual_core_from_physical_core({core.x, core.y});
@@ -1070,6 +1080,7 @@ void Device::init_command_queue_device() {
             sub_device_manager_tracker_->get_active_sub_device_manager()->num_sub_devices(),
             sub_device_manager_tracker_->get_active_sub_device_manager()->noc_mcast_unicast_data());
     }
+    std::cout << "done init cq device" << std::endl;
 }
 
 void Device::init_fabric() {
@@ -1266,6 +1277,8 @@ CoreCoord Device::compute_with_storage_grid_size() const {
 }
 
 CoreCoord Device::virtual_noc0_coordinate(uint8_t noc_index, CoreCoord coord) const {
+    // what happens if virtual and physical overlap ...
+    return coord;
     if (coord.x >= this->grid_size().x || coord.y >= this->grid_size().y) {
         // Coordinate already in virtual space: NOC0 and NOC1 are the same
         return coord;
@@ -1764,6 +1777,7 @@ std::vector<std::pair<transfer_info_cores, uint32_t>> Device::extract_dst_noc_mu
     for (const CoreRange& core_range : ranges) {
         CoreCoord virtual_start = this->virtual_core_from_logical_core(core_range.start_coord, core_type);
         CoreCoord virtual_end = this->virtual_core_from_logical_core(core_range.end_coord, core_type);
+        std::cout << "virtual st " << virtual_start.str() << " , virtual end " << virtual_end.str() << std::endl;
 
         uint32_t num_receivers = core_range.size();
         dst_noc_multicast_info.push_back(std::make_pair(CoreRange(virtual_start, virtual_end), num_receivers));
