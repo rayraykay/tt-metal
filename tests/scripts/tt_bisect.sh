331d0a8ad4c3ddf1d42efadfb49db1dff9eeb410
Andrew Fuller <afuller@tenstorrent.com>
2025-02-24 23:46:42 +0000

    ff

--- tests/scripts/tt_bisect.sh
+++ tests/scripts/tt_bisect.sh
@@ -69,7 +69,8 @@ while [[ "$found" = "false" ]]; do
    fi
    git submodule update --recursive
    build_rc=0
-   ./build_metal.sh --build-tests > /dev/null || build_rc=$?
+   # ./build_metal.sh --build-tests > /dev/null || build_rc=$?
+   build_rc=99 # FIXME
    echo "::endgroup::"
 
    if [[ $build_rc -ne 0 ]]; then
