df5676ba526a75418c90c5bc64a3d2568b3bb9ad
vguduruTT <venkatesh.guduru@multicorewareinc.com>
2025-03-13 05:41:37 +0000

    #0:Add Segformer Classification Demo

--- tests/scripts/single_card/run_single_card_demo_tests.sh
+++ tests/scripts/single_card/run_single_card_demo_tests.sh
@@ -19,6 +19,16 @@ run_qwen7b_func() {
 
 }
 
+run_segformer_func() {
+
+  #Segformer Segmentation Demo
+  WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest --disable-warnings models/demos/segformer/demo/demo_for_semantic_segmentation.py --timeout 600; fail+=$?
+
+  #Segformer Classification Demo
+  WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest --disable-warnings models/demos/segformer/demo/demo_for_image_classification.py --timeout 600; fail+=$?
+
+}
+
 run_llama3_func() {
   fail=0
 
@@ -182,9 +192,6 @@ run_common_func_tests() {
 
   #RoBERTa
   pytest --disable-warnings models/demos/roberta/demo/demo.py --timeout 600; fail+=$?
-
-  #Segformer Demo
-  WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest --disable-warnings models/demos/segformer/demo/demo_for_semantic_segmentation.py --timeout 600; fail+=$?
 
   return $fail
 }
