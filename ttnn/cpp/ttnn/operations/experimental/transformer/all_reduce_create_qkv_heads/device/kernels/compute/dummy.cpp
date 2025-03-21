a2a3d474b52093faa8805b1e02f8d3378be7f14e
Ligang Long <llong@tenstorrent.com>
2025-03-21 23:08:07 +0000

    create dummy kernels and use it in the program factory, to avoid crashing the kernel at this point, builds and runs well

---
+++ ttnn/cpp/ttnn/operations/experimental/transformer/all_reduce_create_qkv_heads/device/kernels/compute/dummy.cpp
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <cstdint>
+#include "compute_kernel_api/eltwise_binary.h"
+
+namespace NAMESPACE {
+void MAIN {}
+}  // namespace NAMESPACE
