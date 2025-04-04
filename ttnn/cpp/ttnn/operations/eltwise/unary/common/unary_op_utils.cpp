9ca5953b07c57b78dbeefcb185ca13250c19f131
Aswinmcw <azayasankaran@tenstorrent.com>
2025-04-04 09:24:56 +0000

    Fix existing case

--- ttnn/cpp/ttnn/operations/eltwise/unary/common/unary_op_utils.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary/common/unary_op_utils.cpp
@@ -65,6 +65,8 @@ std::string get_macro_definition(UnaryOpType op_type) {
         case UnaryOpType::FMOD: return "SFPU_OP_FMOD_INCLUDE";
         case UnaryOpType::FILL: return "SFPU_OP_FILL_INCLUDE";
         case UnaryOpType::ROUND: return "SFPU_OP_ROUND_INCLUDE";
+        case UnaryOpType::EQZ:
+        case UnaryOpType::UNARY_NE: return "SFPU_OP_COMP_INT_INCLUDE";
         default: return "SFPU_OP_COMPUTE_KERNEL_API_INCLUDE";
     };
 }
@@ -214,9 +216,6 @@ std::pair<std::string, std::string> get_op_init_and_func_parameterized(
                     "unary_ne_tile_init();",
                     fmt::format("unary_ne_tile({}, {:#x}u);", idst, std::bit_cast<uint32_t>(param0))};
             }
-            // op_init_and_name = {
-            // "unary_ne_tile_init();",
-            // fmt::format("unary_ne_tile({}, {:#x}u);", idst, std::bit_cast<uint32_t>(param0))};
 
             break;
         case UnaryOpType::UNARY_GT:
