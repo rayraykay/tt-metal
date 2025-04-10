230470239cf4v7r09237m/tt-metal-newci

#17785: Swin_S ttnn implementation

tests/ttnn/integration_tests/swin_s/test_ttnn_mlp.py
@@ -1,0 +1,99 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from torch import nn
+import pytest
+from ttnn.model_preprocessing import (
+    preprocess_model_parameters,
+    preprocess_linear_weight,
+    preprocess_layernorm_parameter,
+    preprocess_linear_bias,
+)
+from models.utility_functions import skip_for_grayskull
+from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.experimental.functional_swin_s.reference.mlp import MLP
+from models.experimental.functional_swin_s.tt.tt_mlp import TtMLP
+from torchvision import models
+import ttnn
+
+
+def create_custom_preprocessor(device):
+    def custom_preprocessor(torch_model, name, ttnn_module_args):
+        parameters = {}
+        if isinstance(torch_model, MLP):
+            parameters[0] = {}
+            parameters[3] = {}
+            parameters[0]["weight"] = preprocess_linear_weight(torch_model[0].weight, dtype=ttnn.bfloat16)
+            parameters[0]["bias"] = preprocess_linear_bias(torch_model[0].bias, dtype=ttnn.bfloat16)
+            parameters[3]["weight"] = preprocess_linear_weight(torch_model[3].weight, dtype=ttnn.bfloat16)
+            parameters[3]["bias"] = preprocess_linear_bias(torch_model[3].bias, dtype=ttnn.bfloat16)
+            if torch.layer_norm in torch_model:
+                parameters[1] = {}
+                parameters[1]["weight"] = preprocess_layernorm_parameter(torch_model[1].weight, dtype=ttnn.bfloat16)
+                parameters[1]["bias"] = preprocess_layernorm_parameter(torch_model[1].bias, dtype=ttnn.bfloat16)
+        return parameters
+
+    return custom_preprocessor
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size",
+    [
+        1,
+        # 8, #MM shard is given for bs=1 alone in the pipeline
+    ],
+)
+@pytest.mark.parametrize(
+    "in_channels,hidden_channels,seq_len,i,j",
+    [
+        (96, [384, 96], 128, 1, 0),
+        (192, [768, 192], 64, 3, 0),
+        (384, [1536, 384], 32, 5, 0),
+        (768, [3072, 768], 16, 7, 0),
+    ],
+)
+def test_mlp(device, in_channels, hidden_channels, batch_size, seq_len, i, j, reset_seeds):
+    model = models.swin_s(weights="IMAGENET1K_V1")
+    state_dict = state_dict = model.state_dict()
+    mlp_state_dict = {k: v for k, v in state_dict.items() if (k.startswith(f"features.{i}.{j}.mlp."))}
+
+    if not mlp_state_dict:
+        raise ValueError("No parameters found in mlp_state_dict")
+
+    torch_model = MLP(in_channels, hidden_channels, activation_layer=nn.GELU)
+
+    new_state_dict = {}
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in mlp_state_dict.items()]
+
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    # Input tensor for testing
+    torch_input_tensor = torch.randn(batch_size, seq_len, seq_len, in_channels)  # Sample input tensor
+    torch_output_tensor = torch_model(torch_input_tensor)
+
+    parameters = preprocess_model_parameters(
+        initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor(device), device=device
+    )
+
+    # Convert the model to TTNN
+    ttnn_model = TtMLP(hidden_channels, device, parameters, activation_layer=ttnn.gelu)
+
+    # Convert input tensor to TTNN format
+    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+
+    # Apply TTNN model
+    output_tensor = ttnn_model(input_tensor)
+
+    # Convert output tensor back to Torch format
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)

tests/ttnn/integration_tests/swin_s/test_ttnn_patchmerging.py
@@ -1,0 +1,96 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from torchvision import models
+import pytest
+import ttnn
+from ttnn.model_preprocessing import (
+    preprocess_model_parameters,
+    preprocess_linear_weight,
+    preprocess_layernorm_parameter,
+)
+from models.utility_functions import skip_for_grayskull
+from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.experimental.functional_swin_s.reference.patchmerging import PatchMerging
+from models.experimental.functional_swin_s.tt.tt_patchmerging import TtPatchMerging
+
+
+def create_custom_preprocessor(device):
+    def custom_preprocessor(torch_model, name, ttnn_module_args):
+        parameters = {}
+        if isinstance(torch_model, PatchMerging):
+            parameters["reduction"] = {}
+            parameters["reduction"]["weight"] = preprocess_linear_weight(
+                torch_model.reduction.weight, dtype=ttnn.bfloat16
+            )
+            parameters["norm"] = {}
+            parameters["norm"]["weight"] = preprocess_layernorm_parameter(torch_model.norm.weight, dtype=ttnn.bfloat16)
+            parameters["norm"]["bias"] = preprocess_layernorm_parameter(torch_model.norm.bias, dtype=ttnn.bfloat16)
+        return parameters
+
+    return custom_preprocessor
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size",
+    [
+        1,
+        # 8, #MM shard is given for bs=1 alone in the pipeline
+    ],
+)
+@pytest.mark.parametrize(
+    "dim,seq_len,i",
+    [
+        (96, 128, 2),
+        (192, 64, 4),
+        (384, 32, 6),
+    ],
+)
+def test_patchmerging(device, batch_size, dim, seq_len, i, reset_seeds):
+    model = models.swin_s(weights="IMAGENET1K_V1")
+    state_dict = state_dict = model.state_dict()
+    patchmerging_state_dict = {k: v for k, v in state_dict.items() if (k.startswith(f"features.{i}."))}
+
+    if not patchmerging_state_dict:
+        raise ValueError("No parameters found in resblock_state_dict")
+
+    torch_model = PatchMerging(dim)
+    for layer in torch_model.children():
+        print(layer)
+
+    new_state_dict = {}
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in patchmerging_state_dict.items()]
+
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    # Input tensor for testing
+    torch_input_tensor = torch.randn(batch_size, seq_len, seq_len, dim)  # Sample input tensor
+    torch_output_tensor = torch_model(torch_input_tensor)
+
+    # Preprocess the model parameters for TTNN
+    parameters = preprocess_model_parameters(
+        initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor(device), device=device
+    )
+
+    # Convert the model to TTNN
+    ttnn_model = TtPatchMerging(device, parameters, dim)
+
+    # Convert input tensor to TTNN format
+    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+
+    # Apply TTNN model
+    output_tensor = ttnn_model(input_tensor)
+
+    # Convert output tensor back to Torch format
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)

tests/ttnn/integration_tests/swin_s/test_ttnn_shifted_window_attention.py
@@ -1,0 +1,145 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from torchvision import models
+import pytest
+from models.utility_functions import skip_for_grayskull
+from models.experimental.functional_swin_s.reference.shifted_window_attention import ShiftedWindowAttention
+from models.experimental.functional_swin_s.tt.tt_shifted_window_attention import TtShiftedWindowAttention
+from tests.ttnn.utils_for_testing import assert_with_pcc
+import ttnn
+from ttnn.model_preprocessing import preprocess_model_parameters, preprocess_linear_weight, preprocess_linear_bias
+
+
+def preprocess_attn_mask(input_shape, patch_size, window_size, shift_size, device):
+    h, w = input_shape[2], input_shape[3]
+    attention_h = ((h - (patch_size[0] - 1) - 1) // patch_size[0]) + 1
+    attention_w = ((w - (patch_size[0] - 1) - 1) // patch_size[0]) + 1
+    attn_mask_tuple = ()
+    for i in range(4):
+        pad_r = (window_size[1] - attention_w % window_size[1]) % window_size[1]
+        pad_b = (window_size[0] - attention_h % window_size[0]) % window_size[0]
+        pad_H = attention_h + pad_b
+        pad_W = attention_w + pad_r
+        num_windows = (pad_H // window_size[0]) * (pad_W // window_size[1])
+
+        attn_mask = torch.zeros((pad_H, pad_W))
+        h_slices = (
+            (0, -window_size[0]),
+            (-window_size[0], -shift_size[0]),
+            (-shift_size[0], None),
+        )
+        w_slices = (
+            (0, -window_size[1]),
+            (-window_size[1], -shift_size[1]),
+            (-shift_size[1], None),
+        )
+        count = 0
+        for h in h_slices:
+            for w in w_slices:
+                attn_mask[h[0] : h[1], w[0] : w[1]] = count
+                count += 1
+        attn_mask = attn_mask.view(pad_H // window_size[0], window_size[0], pad_W // window_size[1], window_size[1])
+        attn_mask = attn_mask.permute(0, 2, 1, 3).reshape(num_windows, window_size[0] * window_size[1])
+        attn_mask = attn_mask.unsqueeze(1) - attn_mask.unsqueeze(2)
+        attn_mask = attn_mask.masked_fill(attn_mask != 0, float(-100.0)).masked_fill(attn_mask == 0, float(0.0))
+        attn_mask = attn_mask.unsqueeze(1).unsqueeze(0)
+        attn_mask = ttnn.from_torch(attn_mask, device=device, layout=ttnn.TILE_LAYOUT)
+        attention_h = attention_h // 2
+        attention_w = attention_w // 2
+
+        attn_mask_tuple += (attn_mask,)
+
+    return attn_mask_tuple
+
+
+def create_custom_preprocessor(device):
+    def custom_preprocessor(torch_model, name, ttnn_module_args):
+        parameters = {}
+        if isinstance(torch_model, ShiftedWindowAttention):
+            parameters["qkv"] = {}
+            parameters["proj"] = {}
+            parameters["qkv"]["weight"] = preprocess_linear_weight(torch_model.qkv.weight, dtype=ttnn.bfloat16)
+            parameters["qkv"]["bias"] = preprocess_linear_bias(torch_model.qkv.bias, dtype=ttnn.bfloat16)
+            parameters["relative_position_bias"] = ttnn.from_torch(
+                torch_model.get_relative_position_bias(), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT
+            )
+            parameters["proj"]["weight"] = preprocess_linear_weight(torch_model.proj.weight, dtype=ttnn.bfloat16)
+            parameters["proj"]["bias"] = preprocess_linear_bias(torch_model.proj.bias, dtype=ttnn.bfloat16)
+
+        return parameters
+
+    return custom_preprocessor
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size",
+    [
+        1,
+        # 8,
+    ],
+)
+@pytest.mark.parametrize(
+    "dim,window_size,shift_size,num_heads,seq_len,i,j",
+    [
+        (96, [7, 7], [0, 0], 3, 128, 1, 0),
+        (96, [7, 7], [3, 3], 3, 128, 1, 1),  # Low pcc after precomputing attn_mask outside, check it
+        (192, [7, 7], [0, 0], 6, 64, 3, 0),
+        (192, [7, 7], [3, 3], 6, 64, 3, 1),  # Low pcc after precomputing attn_mask outside, check it
+        (384, [7, 7], [0, 0], 12, 32, 5, 0),
+        (384, [7, 7], [3, 3], 12, 32, 5, 1),  # Low pcc after precomputing attn_mask outside, check it
+        (768, [7, 7], [0, 0], 24, 16, 7, 0),
+        (768, [7, 7], [3, 3], 24, 16, 7, 1),  # Low pcc after precomputing attn_mask outside, check it
+    ],
+)
+def test_shifted_window_attention(
+    device, batch_size, dim, window_size, shift_size, num_heads, seq_len, i, j, reset_seeds
+):
+    model = models.swin_s(weights="IMAGENET1K_V1")
+    state_dict = model.state_dict()
+    ds_state_dict = {k: v for k, v in state_dict.items() if (k.startswith(f"features.{i}.{j}.attn."))}
+
+    torch_model = ShiftedWindowAttention(dim, window_size, shift_size, num_heads)
+    print(torch_model)
+
+    attn_mask_tuple = preprocess_attn_mask([1, 3, 512, 512], [4, 4], [7, 7], [3, 3], device)
+
+    if seq_len == 128:
+        attn_mask = attn_mask_tuple[0]
+    elif seq_len == 64:
+        attn_mask = attn_mask_tuple[1]
+    elif seq_len == 32:
+        attn_mask = attn_mask_tuple[2]
+    elif seq_len == 16:
+        attn_mask = attn_mask_tuple[3]
+
+    new_state_dict = {}
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in ds_state_dict.items()]
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    torch_input_tensor = torch.randn(batch_size, seq_len, seq_len, dim)
+    torch_output = torch_model(torch_input_tensor)
+
+    parameters = preprocess_model_parameters(
+        initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor(device), device=device
+    )
+
+    ttnn_model = TtShiftedWindowAttention(
+        parameters, device, dim, window_size, shift_size, num_heads, attn_mask=attn_mask
+    )
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device)
+
+    tt_output = ttnn_model(input_tensor)
+
+    tt_output = ttnn.to_torch(tt_output)
+    assert_with_pcc(torch_output, tt_output, 0.99)

rayraykay/tt-metal

tests/ttnn/integration_tests/swin_s/test_ttnn_mlp.py
@@ -1,0 +1,99 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from torch import nn
+import pytest
+from ttnn.model_preprocessing import (
+    preprocess_model_parameters,
+    preprocess_linear_weight,
+    preprocess_layernorm_parameter,
+    preprocess_linear_bias,
+)
+from models.utility_functions import skip_for_grayskull
+from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.experimental.swin_s.reference.mlp import MLP
+from models.experimental.swin_s.tt.tt_mlp import TtMLP
+from torchvision import models
+import ttnn
+
+
+def create_custom_preprocessor(device):
+    def custom_preprocessor(torch_model, name, ttnn_module_args):
+        parameters = {}
+        if isinstance(torch_model, MLP):
+            parameters[0] = {}
+            parameters[3] = {}
+            parameters[0]["weight"] = preprocess_linear_weight(torch_model[0].weight, dtype=ttnn.bfloat16)
+            parameters[0]["bias"] = preprocess_linear_bias(torch_model[0].bias, dtype=ttnn.bfloat16)
+            parameters[3]["weight"] = preprocess_linear_weight(torch_model[3].weight, dtype=ttnn.bfloat16)
+            parameters[3]["bias"] = preprocess_linear_bias(torch_model[3].bias, dtype=ttnn.bfloat16)
+            if torch.layer_norm in torch_model:
+                parameters[1] = {}
+                parameters[1]["weight"] = preprocess_layernorm_parameter(torch_model[1].weight, dtype=ttnn.bfloat16)
+                parameters[1]["bias"] = preprocess_layernorm_parameter(torch_model[1].bias, dtype=ttnn.bfloat16)
+        return parameters
+
+    return custom_preprocessor
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size",
+    [
+        1,
+        # 8, #MM shard is given for bs=1 alone in the pipeline
+    ],
+)
+@pytest.mark.parametrize(
+    "in_channels,hidden_channels,seq_len,i,j",
+    [
+        (96, [384, 96], 128, 1, 0),
+        (192, [768, 192], 64, 3, 0),
+        (384, [1536, 384], 32, 5, 0),
+        (768, [3072, 768], 16, 7, 0),
+    ],
+)
+def test_mlp(device, in_channels, hidden_channels, batch_size, seq_len, i, j, reset_seeds):
+    model = models.swin_s(weights="IMAGENET1K_V1")
+    state_dict = state_dict = model.state_dict()
+    mlp_state_dict = {k: v for k, v in state_dict.items() if (k.startswith(f"features.{i}.{j}.mlp."))}
+
+    if not mlp_state_dict:
+        raise ValueError("No parameters found in mlp_state_dict")
+
+    torch_model = MLP(in_channels, hidden_channels, activation_layer=nn.GELU)
+
+    new_state_dict = {}
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in mlp_state_dict.items()]
+
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    # Input tensor for testing
+    torch_input_tensor = torch.randn(batch_size, seq_len, seq_len, in_channels)  # Sample input tensor
+    torch_output_tensor = torch_model(torch_input_tensor)
+
+    parameters = preprocess_model_parameters(
+        initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor(device), device=device
+    )
+
+    # Convert the model to TTNN
+    ttnn_model = TtMLP(hidden_channels, device, parameters, activation_layer=ttnn.gelu)
+
+    # Convert input tensor to TTNN format
+    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+
+    # Apply TTNN model
+    output_tensor = ttnn_model(input_tensor)
+
+    # Convert output tensor back to Torch format
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)

tests/ttnn/integration_tests/swin_s/test_ttnn_patchmerging.py
@@ -1,0 +1,94 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from torchvision import models
+import pytest
+import ttnn
+from ttnn.model_preprocessing import (
+    preprocess_model_parameters,
+    preprocess_linear_weight,
+    preprocess_layernorm_parameter,
+)
+from models.utility_functions import skip_for_grayskull
+from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.experimental.swin_s.reference.patchmerging import PatchMerging
+from models.experimental.swin_s.tt.tt_patchmerging import TtPatchMerging
+
+
+def create_custom_preprocessor(device):
+    def custom_preprocessor(torch_model, name, ttnn_module_args):
+        parameters = {}
+        if isinstance(torch_model, PatchMerging):
+            parameters["reduction"] = {}
+            parameters["reduction"]["weight"] = preprocess_linear_weight(
+                torch_model.reduction.weight, dtype=ttnn.bfloat16
+            )
+            parameters["norm"] = {}
+            parameters["norm"]["weight"] = preprocess_layernorm_parameter(torch_model.norm.weight, dtype=ttnn.bfloat16)
+            parameters["norm"]["bias"] = preprocess_layernorm_parameter(torch_model.norm.bias, dtype=ttnn.bfloat16)
+        return parameters
+
+    return custom_preprocessor
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size",
+    [
+        1,
+        # 8, #MM shard is given for bs=1 alone in the pipeline
+    ],
+)
+@pytest.mark.parametrize(
+    "dim,seq_len,i",
+    [
+        (96, 128, 2),
+        (192, 64, 4),
+        (384, 32, 6),
+    ],
+)
+def test_patchmerging(device, batch_size, dim, seq_len, i, reset_seeds):
+    model = models.swin_s(weights="IMAGENET1K_V1")
+    state_dict = state_dict = model.state_dict()
+    patchmerging_state_dict = {k: v for k, v in state_dict.items() if (k.startswith(f"features.{i}."))}
+
+    if not patchmerging_state_dict:
+        raise ValueError("No parameters found in resblock_state_dict")
+
+    torch_model = PatchMerging(dim)
+
+    new_state_dict = {}
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in patchmerging_state_dict.items()]
+
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    # Input tensor for testing
+    torch_input_tensor = torch.randn(batch_size, seq_len, seq_len, dim)  # Sample input tensor
+    torch_output_tensor = torch_model(torch_input_tensor)
+
+    # Preprocess the model parameters for TTNN
+    parameters = preprocess_model_parameters(
+        initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor(device), device=device
+    )
+
+    # Convert the model to TTNN
+    ttnn_model = TtPatchMerging(device, parameters, dim)
+
+    # Convert input tensor to TTNN format
+    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+
+    # Apply TTNN model
+    output_tensor = ttnn_model(input_tensor)
+
+    # Convert output tensor back to Torch format
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)

tests/ttnn/integration_tests/swin_s/test_ttnn_shifted_window_attention.py
@@ -1,0 +1,144 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from torchvision import models
+import pytest
+from models.utility_functions import skip_for_grayskull
+from models.experimental.swin_s.reference.shifted_window_attention import ShiftedWindowAttention
+from models.experimental.swin_s.tt.tt_shifted_window_attention import TtShiftedWindowAttention
+from tests.ttnn.utils_for_testing import assert_with_pcc
+import ttnn
+from ttnn.model_preprocessing import preprocess_model_parameters, preprocess_linear_weight, preprocess_linear_bias
+
+
+def preprocess_attn_mask(input_shape, patch_size, window_size, shift_size, device):
+    h, w = input_shape[2], input_shape[3]
+    attention_h = ((h - (patch_size[0] - 1) - 1) // patch_size[0]) + 1
+    attention_w = ((w - (patch_size[0] - 1) - 1) // patch_size[0]) + 1
+    attn_mask_tuple = ()
+    for i in range(4):
+        pad_r = (window_size[1] - attention_w % window_size[1]) % window_size[1]
+        pad_b = (window_size[0] - attention_h % window_size[0]) % window_size[0]
+        pad_H = attention_h + pad_b
+        pad_W = attention_w + pad_r
+        num_windows = (pad_H // window_size[0]) * (pad_W // window_size[1])
+
+        attn_mask = torch.zeros((pad_H, pad_W))
+        h_slices = (
+            (0, -window_size[0]),
+            (-window_size[0], -shift_size[0]),
+            (-shift_size[0], None),
+        )
+        w_slices = (
+            (0, -window_size[1]),
+            (-window_size[1], -shift_size[1]),
+            (-shift_size[1], None),
+        )
+        count = 0
+        for h in h_slices:
+            for w in w_slices:
+                attn_mask[h[0] : h[1], w[0] : w[1]] = count
+                count += 1
+        attn_mask = attn_mask.view(pad_H // window_size[0], window_size[0], pad_W // window_size[1], window_size[1])
+        attn_mask = attn_mask.permute(0, 2, 1, 3).reshape(num_windows, window_size[0] * window_size[1])
+        attn_mask = attn_mask.unsqueeze(1) - attn_mask.unsqueeze(2)
+        attn_mask = attn_mask.masked_fill(attn_mask != 0, float(-100.0)).masked_fill(attn_mask == 0, float(0.0))
+        attn_mask = attn_mask.unsqueeze(1).unsqueeze(0)
+        attn_mask = ttnn.from_torch(attn_mask, device=device, layout=ttnn.TILE_LAYOUT)
+        attention_h = attention_h // 2
+        attention_w = attention_w // 2
+
+        attn_mask_tuple += (attn_mask,)
+
+    return attn_mask_tuple
+
+
+def create_custom_preprocessor(device):
+    def custom_preprocessor(torch_model, name, ttnn_module_args):
+        parameters = {}
+        if isinstance(torch_model, ShiftedWindowAttention):
+            parameters["qkv"] = {}
+            parameters["proj"] = {}
+            parameters["qkv"]["weight"] = preprocess_linear_weight(torch_model.qkv.weight, dtype=ttnn.bfloat16)
+            parameters["qkv"]["bias"] = preprocess_linear_bias(torch_model.qkv.bias, dtype=ttnn.bfloat16)
+            parameters["relative_position_bias"] = ttnn.from_torch(
+                torch_model.get_relative_position_bias(), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT
+            )
+            parameters["proj"]["weight"] = preprocess_linear_weight(torch_model.proj.weight, dtype=ttnn.bfloat16)
+            parameters["proj"]["bias"] = preprocess_linear_bias(torch_model.proj.bias, dtype=ttnn.bfloat16)
+
+        return parameters
+
+    return custom_preprocessor
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size",
+    [
+        1,
+        # 8,
+    ],
+)
+@pytest.mark.parametrize(
+    "dim,window_size,shift_size,num_heads,seq_len,i,j",
+    [
+        (96, [7, 7], [0, 0], 3, 128, 1, 0),
+        (96, [7, 7], [3, 3], 3, 128, 1, 1),  # Low pcc after precomputing attn_mask outside, check it
+        (192, [7, 7], [0, 0], 6, 64, 3, 0),
+        (192, [7, 7], [3, 3], 6, 64, 3, 1),  # Low pcc after precomputing attn_mask outside, check it
+        (384, [7, 7], [0, 0], 12, 32, 5, 0),
+        (384, [7, 7], [3, 3], 12, 32, 5, 1),  # Low pcc after precomputing attn_mask outside, check it
+        (768, [7, 7], [0, 0], 24, 16, 7, 0),
+        (768, [7, 7], [3, 3], 24, 16, 7, 1),  # Low pcc after precomputing attn_mask outside, check it
+    ],
+)
+def test_shifted_window_attention(
+    device, batch_size, dim, window_size, shift_size, num_heads, seq_len, i, j, reset_seeds
+):
+    model = models.swin_s(weights="IMAGENET1K_V1")
+    state_dict = model.state_dict()
+    ds_state_dict = {k: v for k, v in state_dict.items() if (k.startswith(f"features.{i}.{j}.attn."))}
+
+    torch_model = ShiftedWindowAttention(dim, window_size, shift_size, num_heads)
+
+    attn_mask_tuple = preprocess_attn_mask([1, 3, 512, 512], [4, 4], [7, 7], [3, 3], device)
+
+    if seq_len == 128:
+        attn_mask = attn_mask_tuple[0]
+    elif seq_len == 64:
+        attn_mask = attn_mask_tuple[1]
+    elif seq_len == 32:
+        attn_mask = attn_mask_tuple[2]
+    elif seq_len == 16:
+        attn_mask = attn_mask_tuple[3]
+
+    new_state_dict = {}
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in ds_state_dict.items()]
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    torch_input_tensor = torch.randn(batch_size, seq_len, seq_len, dim)
+    torch_output = torch_model(torch_input_tensor)
+
+    parameters = preprocess_model_parameters(
+        initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor(device), device=device
+    )
+
+    ttnn_model = TtShiftedWindowAttention(
+        parameters, device, dim, window_size, shift_size, num_heads, attn_mask=attn_mask
+    )
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device)
+
+    tt_output = ttnn_model(input_tensor)
+
+    tt_output = ttnn.to_torch(tt_output)
+    assert_with_pcc(torch_output, tt_output, 0.99)