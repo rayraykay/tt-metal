c40a314206d8315fcb1739a1a4d298843514bffe
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-04-10 14:21:15 +0000

    WIP: null kernel

--- tests/tt_metal/tt_metal/test_kernels/sfpi/a.cpp
+++ tests/tt_metal/tt_metal/test_kernels/sfpi/a.cpp
@@ -1,1 +1,8 @@
-bob
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "compute_kernel_api/common.h"
+namespace NAMESPACE {
+void MAIN {}
+}  // namespace NAMESPACE
