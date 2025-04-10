bd7567b64e4f2db7e70c3d5bf124d33b92dc9ad6
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-04-10 13:24:29 +0000

    WIP: test compiles

--- tests/tt_metal/tt_metal/sfpi/test_sfpi.cpp
+++ tests/tt_metal/tt_metal/sfpi/test_sfpi.cpp
@@ -2,25 +2,32 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+#include "command_queue_fixture.hpp"
+
+#include <tt-metalium/core_coord.hpp>
+#include <tt-metalium/host_api.hpp>
+#include <tt-metalium/kernel.hpp>
+#include <tt-metalium/kernel_types.hpp>
+
 #include <algorithm>
+#include <cstdio>
 #include <filesystem>
 #include <string>
 #include <string_view>
+
 #include <gtest/gtest.h>
-#include <cstdio>
 
 static constexpr std::string_view KernelDir = "tests/tt_metal/tt_metal/test_kernels/sfpi/";
 
-static bool runTest(const std::string& path, unsigned baseLen) {
+static bool runTest(tt::tt_metal::IDevice* device, const CoreCoord& coord, const std::string& path, unsigned baseLen) {
     const char* phase;
 
-    // compile
-    phase = "compile";
-    // enqueue
-    phase = "queue";
-    // execute
-    phase = "execute";
-    // results
+    // FIXME: Should we build all these first, and then run them?
+    auto program(tt::tt_metal::CreateProgram());
+    auto kernel = CreateKernel(program, path, coord, tt::tt_metal::ComputeConfig{});
+    EnqueueProgram(device->command_queue(), program, false);
+    Finish(device->command_queue());
+
     bool pass = true;
     std::printf("%s: %s\n", path.c_str() + baseLen + 1, pass ? "PASSED" : "FAILED");
     return pass;
@@ -30,7 +37,8 @@ failed:
     return false;
 }
 
-static bool runTests(std::string& path, unsigned baseLen) {
+static bool runTests(
+    tt::tt_metal::IDevice* device, const tt::tt_metal::CoreCoord coord, std::string& path, unsigned baseLen) {
     bool pass = true;
     std::vector<std::string> files;
     std::vector<std::string> dirs;
@@ -38,7 +46,7 @@ static bool runTests(std::string& path, unsigned baseLen) {
     for (const auto& entry : std::filesystem::directory_iterator(path)) {
         if (entry.is_directory()) {
             dirs.push_back(entry.path().filename());
-        } else {
+        } else if (entry.path().filename().extension() == ".cpp") {
             files.push_back(entry.path().filename());
         }
     }
@@ -48,13 +56,13 @@ static bool runTests(std::string& path, unsigned baseLen) {
     path.push_back('/');
     for (const auto& file : files) {
         path.append(file);
-        pass &= runTest(path, baseLen);
+        pass &= runTest(device, coord, path, baseLen);
         path.erase(path.size() - file.size());
     }
 
     for (const auto& dir : dirs) {
         path.append(dir);
-        pass &= runTests(path, baseLen);
+        pass &= runTests(device, coord, path, baseLen);
         path.erase(path.size() - dir.size());
     }
     path.pop_back();
@@ -62,19 +70,23 @@ static bool runTests(std::string& path, unsigned baseLen) {
     return pass;
 }
 
-static bool runTestsuite() {
-    bool pass = true;
-
-    // Setup
-
-    // Iterate
-    std::string path(KernelDir);
-    // FIXME: how to find this path relative to pwd?
-    pass &= runTests(path, path.size());
-
-    // Teardown
-
-    return pass;
+static bool runTestsuite(tt::tt_metal::IDevice* device, const tt::tt_metal::CoreCoord coord) {
+    std::string path;
+    if (auto* var = std::getenv("TT_METAL_HOME")) {
+        path.append(var);
+        if (!path.empty()) {
+            path.push_back('/');
+        }
+    }
+    path.append(KernelDir);
+    return runTests(device, coord, path, path.size());
 }
 
-TEST(SFPI, TensixSFPI) { EXPECT_TRUE(runTestsuite()); }
+using tt::tt_metal::CommandQueueSingleCardProgramFixture;
+
+TEST_F(CommandQueueSingleCardProgramFixture, TensixSFPI) {
+    CoreCoord core{0, 0};
+    for (auto* device : devices_) {
+        EXPECT_TRUE(runTestsuite(device, core));
+    }
+}
